8bb920cd511488872cf210b26cc0cb26e263f554
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-23 23:09:04 +0000

    Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*prints Holberton
+*
+*Return (0) 
+*/
+int main(void)  
+{
+	_putchar(72);
+	_putchar(111);
+	_putchar(108);
+	_putchar(98);
+	_putchar(101);
+	_putchar(114);
+	_putchar(116);
+	_putchar(111);
+	_putchar(110);
+
+	_putchar('\n');
+return (0);
+}
