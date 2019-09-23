cf547746dfdb5ba47ed776714448178261189a29
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-23 23:55:06 +0000

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10 (void)
+{
+	char y;
+	int x;
+	x = 0;
+	while (x <= 9) 
+	{
+	for (y = 97 ; y <= 122 ; y++) 	
+	{	
+	_putchar(y);
+	}	
+	x++;
+	_putchar(10);
+	}
+}
+
