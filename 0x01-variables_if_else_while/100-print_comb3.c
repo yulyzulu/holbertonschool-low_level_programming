85088f83e1ad1aa9d72b7993ba27a7b74fffedbf
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-23 16:20:06 +0000

    2 digits not repite

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+int y;
+for (x = '0' ; x <= '9'; x++)
+{
+for (y = (x + 1) ; y <= '9'; y++)
+{
+putchar(x);
+putchar(y);
+if (x < '8' || x < '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
