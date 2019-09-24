18740343d75a0297d262c6258635a8c8721bc8a7
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-24 15:23:47 +0000

    Print alphabet 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-void print_alphabet_x10(void) /* print alphabet*/
+*print_alphabet_x10- Print alphabet 10 .
+*
+* Return: Always 0.
+*/
+void print_alphabet_x10(void)
 {
 	char y;
 	int x;
-	x = 0;
 
+	x = 0;
 	while (x <= 9)
 	{
 	for (y = 97 ; y <= 122 ; y++)
