58c59b1b9070b019e766be8ba4acdaf9ebbdfc07
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-25 00:07:33 +0000

    work in this

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*
+*
+*
+*/
+void print_to_98(int n)
+{
+	int c;
+	c = n;
+
+	for (n = c; n <= 98; n++)
+	{
+	printf(
+	}
+
+
+
+}
+
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*
+*
+*
+*/
+void times_table(void)
+{
+	int a, b, c, x, y, z;
+	x = a * 9;
+	y = b * 9;
+	z = c * 9;
+	
+	for (a = 0; a <= 9; a++);
+	for (b = 0; b <= 9; b++);
+	for (c = 0; c <= 9; c++);
+{
+	_putchar(x);
+	_putchar(y);
+	_putchar(z);
+	_putchar('\n');
+}
+	
+}
+
