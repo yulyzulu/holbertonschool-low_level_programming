d98a04a9774add8daeda03c74a6334ec3145b4e5
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-25 03:56:24 +0000

    print 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+positive_or_negative(int i)
+{
+
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+if (i > 0)
+{
+printf("%d is positive\n", i);
+}
+else if (i == 0)
+{
+printf("%d is zero\n", i);
+}
+else
+{
+printf("%d is negative\n", i);
+}
+return (0);
+}
