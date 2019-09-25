4ca0032fb43a22955ab3437d2693342a8e04672b
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-25 23:02:59 +0000

    table 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,25 +1,39 @@
 #include "holberton.h"
 /**
+*times_table- print table
 *
-*
-*
+*Return: print
 */
 void times_table(void)
 {
-	int a, b, c, x, y, z;
-	x = a * 9;
-	y = b * 9;
-	z = c * 9;
-	
-	for (a = 0; a <= 9; a++);
-	for (b = 0; b <= 9; b++);
-	for (c = 0; c <= 9; c++);
-{
-	_putchar(x);
-	_putchar(y);
-	_putchar(z);
-	_putchar('\n');
-}
-	
-}
+	int a, b, multi, d, e;
 
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+		multi = a * b;
+		if (multi <= 9 && b != 0)
+		{
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar('0' + multi);
+		}
+		else if (b == 0)
+		{
+		_putchar('0' + multi);
+		}
+		else
+		{
+		d = multi / 10;
+		e = multi % 10;
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0' + d);
+		_putchar('0' + e);
+		}
+		}
+	_putchar('\n');
+	}
+}
