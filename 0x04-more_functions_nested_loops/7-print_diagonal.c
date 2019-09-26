7df1f6d68f7f420eedabaf0c5997e00f779fa4ad
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-26 23:57:28 +0000

    print /

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+/**
+*print_diagonal- print n diagonal
+*@n: integer
+*Return: \
+*/
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < n; a++)
+	{
+			for (b = 0; b < n; b++)
+			{
+				if (a > b)
+				{
+					_putchar(' ');
+				}
+				else if (a == b)
+				{
+					_putchar(92);
+				}
+				else
+				{
+				}
+			}
+			_putchar('\n');
+	}
+	}
+}
