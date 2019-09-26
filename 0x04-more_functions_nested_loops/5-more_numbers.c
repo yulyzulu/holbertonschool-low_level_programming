75386beb371f39c42bf11cfbd57bc4d44936b808
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-26 21:40:36 +0000

    print numbers 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,18 +1,18 @@
 #include "holberton.h"
 /**
-*more numbers- print more numbers 0 to 14
+*more_numbers- print more numbers 0 to 14
 *
 *Return: 10 times numbers
 */
 void more_numbers(void)
 {
-	int a, b, c, d; 
+	int a, b, c, d;
 
 	for (b = 0; b <= 9; b++)
 	{
 		for (a = 0 ; a <= 14; a++)
 		{
-	
+
 			if (a > 9)
 			{
 			c = a / 10;
@@ -21,7 +21,7 @@ void more_numbers(void)
 
 		d = a % 10;
 		_putchar(d + '0');
-		
+
 		}
 
 	_putchar('\n');
