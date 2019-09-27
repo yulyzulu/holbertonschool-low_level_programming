ef17fcb6d025fa443596cc859ba40f7fbe8c7631
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-27 04:41:01 +0000

    Ten

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+*print_triangle - print triangle
+*@size: integer
+*
+*/
+void print_triangle(int size)
+{
+	int a;
+	
+	for (a = 0; a < size; a++)
+	{
+	
+	}
+
+
+
+
+}
+
