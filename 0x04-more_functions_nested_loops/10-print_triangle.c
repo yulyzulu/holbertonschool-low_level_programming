ecb41f68a74384ccbf7e41ac39a8aadf87c587b8
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-09-30 03:56:58 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,31 +2,31 @@
 /**
 *print_triangle - print triangle
 *@size: integer
-*
+*Return: triangle
 */
 void print_triangle(int size)
 {
-	int a, b;
+	int a, b, c;
 
-	for (a = size; a <= size; a++)
+	if (size <= 0)
 	{
-		for (b = 0; b <= size; b++)
-		{
-			if (a > b)
-			{
-
-				_putchar(' ');
-			}
-			else if (a == b)
-			{
-				_putchar(35);
-			}
-			else
-			{
-			}
-		}
+		_putchar('\n');
 	}
+	else
+	{
+	for (a = 1; a <= size; a++)
+	{
+		for (b = size - a; b >= 1; b--)
+		{
+			_putchar(' ');
+		}
+		for (c = 1; c <= a; c++)
+		{
+			_putchar(35);
+		}
 
 		_putchar('\n');
+	}
+	}
 }
 
