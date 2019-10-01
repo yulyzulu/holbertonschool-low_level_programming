6def5f99055f91d66d84a7b50b186925bca985c5
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-01 22:39:18 +0000

    array reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+*rev_string- reverse string
+*@s: string pointer
+*Return: reverse string
+*/
+void rev_string(char *s)
+{
+	int i, x, j, y;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	i = i - 1;
+	x = (i) / 2;
+	for (j = 0; j < x ; j++)
+	{
+
+		y = s[j];
+		s[j] = s[i - j];
+		s[i - j] = y;
+
+	}
+}
