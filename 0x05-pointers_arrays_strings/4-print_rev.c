6a123a15d5c30dad762e086d5a9e5f582e268a14
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-01 20:41:48 +0000

    print string reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include"holberton.h"
+/**
+*print_rev- print rev string
+*@s: string
+*Return: string in reverse
+*/
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	for (j = (i - 1); j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+
+}
