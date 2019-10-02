d5eb81c018bd3fb80e3cce63ff5b2430eb13712a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-02 04:58:19 +0000

    pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,11 +3,23 @@
 *_strcpy- copies the string pointed
 *@dest: pointer
 *@src: pointer
-*Return: string
+*Return: pointer to dest
 */
 char *_strcpy(char *dest, char *src)
 {
+	int i, j;
 
+	i = 0;
+	while (src[i] != 0)
+	{
+		i++;
+	}
+	j = 0;
+	while (j <= i)
+	{
+		*(dest + j) = *(src + j);
+		j++;
+	}
 
-
+	return (dest);
 }
