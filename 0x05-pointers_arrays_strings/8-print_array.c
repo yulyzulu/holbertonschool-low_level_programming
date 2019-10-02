06e9e928c0e9e3a94bf5d1387196007b9f96052f
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-02 03:42:54 +0000

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,7 +10,7 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	for (x = 0 ; x < n ; x++)
+	for (x = 0 ; x <= n ; x++)
 	{
 		if (x != (n - 1))
 		{
@@ -18,7 +18,7 @@ void print_array(int *a, int n)
 		}
 		else
 		{
-			printf("%d\n", a[n - 1]);
+			printf("%d\n", a[x]);
 		}
 	}
 }
