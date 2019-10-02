0a4486fc714f9be56f7ebcaf9c5a0f7de623c22c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-02 03:12:52 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+*print_array- print array
+*@a: pointer
+*@n: integer
+*Return: array
+*/
+void print_array(int *a, int n)
+{
+	int x;
+
+	for (x = 0 ; x < n ; x++)
+	{
+		if (x != (n - 1))
+		{
+			printf("%d, ", a[x]);
+		}
+		else
+		{
+			printf("%d\n", a[n - 1]);
+		}
+	}
+}
