d4754f2831a9938320dabf0f5e535740491f0bd7
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-04 21:50:39 +0000

    capitalizate words

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,11 @@ char *cap_string(char *str)
 	int i;
 
 	i = 0;
+
+	if ( str[0] >= 97 &&  str[0] <= 122)
+	{
+		str[0] = str[0] - 32;
+	}
 	while (str[i] != '\0')
 	{
 		if (str[i] == ' ' || str[i] == 9 || str[i] == '\n' || str[i] == ','
@@ -16,12 +21,6 @@ char *cap_string(char *str)
 		|| str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{'
 		|| str[i] == '}')
 		{
-
-			if (str[i] == 9)
-			{
-				str[i] = ' ';
-			}
-
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 			{
 				str[i + 1] = str[i + 1] - 32;
