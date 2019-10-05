48ac3b3277d19616280149c9b3c88764406fb504
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-05 21:46:35 +0000

    change letters to numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,11 +1,31 @@
 #include "holberton.h"
 /**
-*
-*
-*
+**leet- change words to numbers
+*@str: array
+*Return: string with numbers
 */
-char *leet(char *)
+char *leet(char *str)
 {
 
+	int i, j;
+	char letters[] = {"'a','A','e','E','o','O','t','T','l','L'"};
+	char numbers[] = {"'4','4','3','3','0','0','7','7','1','1'"};
 
+	i = 0;
+	while (str[i] != 0)
+	{
+		for (j = 0; j <= i && letters[j] != 0; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = numbers[j];
+				str[0] = numbers[j];
+				str[3] = numbers[j];
+				str[5] = numbers[j];
+			}
+		}
+		i++;
+	}
+
+return (str);
 }
