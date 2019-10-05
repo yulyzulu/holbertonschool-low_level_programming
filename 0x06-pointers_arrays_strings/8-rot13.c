be13b5672730b56b23f32c626f5dbe0cd36da6f3
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-05 23:29:33 +0000

    rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 /**
-*
-*
-*
+**rot13- encodes a string using rot13
+*@str: string
+*Return: string encodes rot13
 */
-char *rot13(char *)
+char *rot13(char *str)
 {
+	int i;
 
-
-
+	i = 0;
+	while (str[i] != 0)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+		str[i] = str[i] + 13;
+		}
+	i++;
+	}
+	return (str);
 }
