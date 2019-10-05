19111f0562a2878de948a2c10c3234afc4bfad1a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-05 22:16:20 +0000

    leet change words to numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,18 +14,15 @@ char *leet(char *str)
 	i = 0;
 	while (str[i] != 0)
 	{
-		for (j = 0; j <= i && letters[j] != 0; j++)
+		for (j = 0; letters[j] <= str[i] && letters[j] != 0 ; j++)
 		{
 			if (str[i] == letters[j])
 			{
 				str[i] = numbers[j];
 				str[0] = numbers[j];
-				str[3] = numbers[j];
-				str[5] = numbers[j];
 			}
 		}
-		i++;
+	i++;
 	}
-
-return (str);
+	return (str);
 }
