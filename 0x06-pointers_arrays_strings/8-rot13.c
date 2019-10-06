c7086b06d809d498129ac85333563f112e4eaf61
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-06 18:57:25 +0000

    encriptation rot 13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,11 +11,17 @@ char *rot13(char *str)
 	i = 0;
 	while (str[i] != 0)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'z') && (str[i] >= 'A' && str[i] <= 'Z'))
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
 		{
-		str[i] = str[i] + 13;
+			str[i] = str[i] + 13;
+		}
+		else if ((str[i] >= 'n' && str[i] <= 'z') ||
+			(str[i] >= 'N' && str[i] <= 'Z'))
+		{
+			str[i] = str[i] - 13;
 		}
 	i++;
+
 	}
 	return (str);
 }
