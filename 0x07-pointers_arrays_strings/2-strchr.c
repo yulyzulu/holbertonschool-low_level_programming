9dec23ed5f51decf38d4b09fbe3de6c0c60d6883
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-07 20:52:41 +0000

    locates a character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+**_strchr- locates a character in a string
+*@s: array
+*@c: char
+*Return: a pointer
+*/
+char *_strchr(char *s, char c)
+{	int i;
+
+	i = 0;
+	while (s[i] != 0)
+	{
+		if (s[i] == c)
+		{
+			s += i;
+			return (s);
+		}
+		i++;
+	}
+
+return (0);
+}
