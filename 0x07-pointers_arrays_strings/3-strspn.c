2911a491b3b57a8c246b71bd50c49fb23ccb9b3a
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-07 23:14:15 +0000

    length

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,16 +7,18 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i = 0, j = 0, x = 0;
+	unsigned int i, j, x;
 
+	i = 0;
+	x = 0;
 	while (s[i] != ' ')
 	{
 		j = 0;
-		while (accept[j] != '\0')
+		while (accept[j] != 0)
 		{
 			if (s[i] == accept[j])
 			{
-			x++;
+				x++;
 			}
 		j++;
 		}
