02a9e9c27b8c0c6ab916f6cb82024675adf016eb
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-13 19:45:58 +0000

    add numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*main- 
-*
-*
+*main- adds positive numbers
+*@argc: integer
+*@argv: array
+*Return: integer
 */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc > 1)
-	{ 
-		for (i = 1; i > argc; i++)
-		{
-		
-			if (i >= '0' && i <= '9')
-						
-			{
-				sum = sum + atoi(argv[i]);
-				printf("%d\n", sum);
-			}
-			else if (argv[i] != (i <= '0' && i >'9')
-			{
-				printf("Error");
-				return (1);
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
 	}
-
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
 }
