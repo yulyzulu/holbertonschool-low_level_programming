64c754190915a05411ae2ae6082bfb1782d20da9
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-17 21:08:59 +0000

    allocate memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+*_calloc- allocates memory for an array
+*@nmemb: unsigned int
+*@size: unsigned int
+*Return: allocates memory for an array
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		p[i] = 0;
+	}
+return (p);
+}
