4db1ec2ac840487966eb6c1c8b99c19d67f8108c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-21 21:15:11 +0000

    print struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,10 +18,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: (nil)\n");
 		}
-		else
-		{
-			printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
-		}
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 
 	}
 
