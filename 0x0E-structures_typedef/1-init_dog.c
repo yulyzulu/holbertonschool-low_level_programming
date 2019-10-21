31de284daebd7efcf18ed4050c782756f03d1296
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-21 20:25:21 +0000

    initialize a variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+*init_dog- inicialize a variable
+*@d: pointer
+*@name: pointer
+*@age: float
+*@owner: pointer
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,7 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 
 #endif
