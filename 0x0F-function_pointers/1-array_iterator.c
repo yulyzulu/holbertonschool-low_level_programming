bd09ccae7af0b5a8c36de71a3604d7a0a5ea0d0b
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-23 17:55:15 +0000

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	unsigned int i;
 
 	if (array != NULL || size > 0 || action != NULL)
-
-	for (i = 0; i < size; i++)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
