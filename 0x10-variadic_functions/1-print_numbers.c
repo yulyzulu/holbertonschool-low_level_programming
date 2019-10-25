7e2cbb5a63c0b3dba44c1cac89b8d66aaf889ead
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-25 19:03:51 +0000

    print

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+*print_numbers- print numbers
+*@separator: char pointer
+*@n: const unsigned int
+*Return: void
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+
+	if (separator != NULL)
+	{
+		va_start(list, n);
+		for (i = 0; i < n; i++)
+		{
+			if (i != (n - 1))
+			{
+				printf("%d%c ", va_arg(list, const unsigned int), separator[0]);
+			}
+			else
+			{
+				 printf("%d", va_arg(list, const unsigned int));
+			}
+
+		}
+		va_end(list);
+	}
+	printf("\n");
+}
