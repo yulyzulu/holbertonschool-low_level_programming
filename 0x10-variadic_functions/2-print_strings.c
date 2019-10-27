acf79de45863ee046e15ffb804a031d58adfd406
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-10-27 02:42:09 +0000

    first p==NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,25 +18,25 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		p = va_arg(list, char *);
 
-		if (separator == NULL)
-		{
-			printf("%s", p);
-		}
-
 		if (p == NULL)
 		{
 			p = "(nill)";
 		}
-		if (separator != NULL)
+
+		if (separator == NULL)
+		{
+			printf("%s", p);
+		}
+		else
 		{
 			if (i < (n - 1))
 			{
 				printf("%s%c ", p, *separator);
 			}
-				else
-				{
-					printf("%s", p);
-				}
+			else
+			{
+				 printf("%s", p);
+			}
 		}
 	}
 	va_end(list);
