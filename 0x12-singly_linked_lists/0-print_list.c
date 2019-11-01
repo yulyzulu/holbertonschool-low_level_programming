52935874d3401e48f5619d459d4f19bbcae79336
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-01 01:27:39 +0000

    new line

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,9 +14,12 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		}
-		printf("[%d] %s\n", h->len, h->str);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
 		h = h->next;
 		i++;
 	}
