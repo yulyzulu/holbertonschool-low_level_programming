1d1ba7a21fc419b2d52b2f2b420e448f65f3e031
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-03 20:36:24 +0000

    divide phrase

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void startupfun(void) __attribute__ ((constructor));
 */
 void startupfun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
