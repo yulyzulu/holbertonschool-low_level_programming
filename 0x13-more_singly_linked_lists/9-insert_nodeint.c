192485b1f329ec57ef1aefcfd0b40d36f99984f0
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-05 19:02:35 +0000

    9 task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,8 +9,36 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new_node, *aux;
+	unsigned int i;
 
+	aux = *head;
+	new_node = malloc(sizeof(listint_t));
 
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	aux = *head;
 
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+	for (i = 0; i < idx; i++)
+	{
+		if (aux == NULL || aux->next == NULL)
+		{
+			return (NULL);
+		}
+		aux = aux->next;
+		i++;
+	}
+	new_node->next = aux->next;
+	aux->next = new_node;
+	return (new_node);
 
 }
