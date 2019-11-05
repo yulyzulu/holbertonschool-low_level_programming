b374fe18456e7ed6712c7154c442dcf57f499aef
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-05 13:57:20 +0000

    if head == NULL, head =  null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 #include <stdio.h>
 /**
-*free_listin2- frees a listin_t list
+*free_listint2- frees a listin_t list
 *@head: double pointer
 */
 void free_listint2(listint_t **head)
 {
 	listint_t *la = *head;
 
+	if (*head == NULL)
+	{
+		return;
+	}
 	while (la != NULL)
 	{
 		free(la);
 		la = la->next;
 		*head = la;
 	}
+	*head = NULL;
 }
