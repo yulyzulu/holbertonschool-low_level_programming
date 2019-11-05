268a12e78be9b148ede85d6da740907a90bfb251
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-05 19:27:01 +0000

    temp variable

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,8 +24,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		new_node->next = aux;
+		aux = new_node;
 		return (new_node);
 	}
 	for (i = 0; i < idx; i++)
