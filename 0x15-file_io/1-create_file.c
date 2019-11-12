17855fdd2583a69193599f85bf8f6ba78da40a09
yulyzulu <yulyzulu05@gmail.com>
2019-11-12 21:41:54 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+#include <unistd.h>
+/**
+*create_file- creates a file
+*@filename: pointer const char
+*@text_content: pointer char
+*Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fil, val1, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	fil = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fil == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[i] != '\0')
+		{
+			i++;
+		}
+	}
+
+	val1 = write(fil, text_content, i);
+		if (val1 == -1)
+			return (-1);
+
+	close(fil);
+	return (1);
+}
