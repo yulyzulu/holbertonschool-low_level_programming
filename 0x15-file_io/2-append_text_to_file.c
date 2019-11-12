037a832fcce0e9f4a7e60360a9afb17c2ae191af
yulyzulu <yulyzulu05@gmail.com>
2019-11-12 22:34:45 +0000

    mode

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fil = open(filename, O_WRONLY, O_APPEND);
+	fil = open(filename, O_WRONLY | O_APPEND);
 
 	if (fil == -1)
 		return (-1);
