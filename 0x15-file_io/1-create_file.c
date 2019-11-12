52089b4e159a30ce5b9adb35e01043bde1710f21
yulyzulu <yulyzulu05@gmail.com>
2019-11-12 21:53:27 +0000

    modification task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,8 +22,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content != NULL)
-	{
-		while (text_content[i] != '\0')
+	{	
+		while (text_content[i])
 		{
 			i++;
 		}
