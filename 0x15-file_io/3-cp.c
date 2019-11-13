494cd571d7fea470843a767a93c78cbbfd0e2960
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-11-13 02:59:20 +0000

    betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,9 +5,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 /**
-*
-*
-*
+*main- copies the content of a file to another file
+*@argc: integer
+*@argv: char pointer
+*Retur
 */
 int main(int argc, char *argv[])
 {
@@ -23,10 +24,9 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);	
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
@@ -38,15 +38,15 @@ int main(int argc, char *argv[])
 		if (write(file_to, buffer, num) != num)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-              		exit(99);
+			exit(99);
 		}
 	}
 	if (num == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-                exit(98);
+		exit(98);
 	}
-	if (close(file_from) == -1 || close (file_to) == -1)
+	if (close(file_from) == -1 || close(file_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
