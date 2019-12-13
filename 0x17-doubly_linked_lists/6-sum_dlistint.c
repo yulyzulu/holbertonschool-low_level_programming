41ca9fdabbf79d183330f355834f55cacf14fcea
yulyzulu <yulyzulu05@gmail.com>
2019-12-13 23:37:10 +0000

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+*sum_dlistint- function that return the sum of all the data of a linked list
+*@head: dlistint pointer
+*Return: the sum of all the data of linked list or 0 if the list is empty
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	dlistint_t *nodes;
+
+	nodes = head;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		while (nodes->next != NULL)
+		{
+			nodes = nodes->next;
+			sum = sum + nodes->n;
+		}
+	}
+return (sum);
+}
