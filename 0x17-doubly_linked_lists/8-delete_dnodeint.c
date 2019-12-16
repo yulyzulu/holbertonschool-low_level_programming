11123b94e2448bfbc199087d12f109ab53d8677c
Yulieth Zuluaga <yulyzulu05@gmail.com>
2019-12-16 02:40:21 +0000

    delete

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,18 +7,33 @@
 */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	int nodes;
-	delistint_t *temp1, *temp2;
-	temp1 = *head;
-	temp2 = *head;
+	dlistint_t *temp1/*, *temp2*/;
+	unsigned int i;
 
-	if (temp1  == NULL && temp2 == NULL)
+	temp1 = *head;
+	if (temp1  == NULL)
+	{
 		return (-1);
-	
+	}
 	if (index == 0)
 	{
 		*head = (*head)->next;
 		free(temp1);
+		(*head)->prev = NULL;
 		return (1);
 	}
+	for (i = 0; i < index - 1; i++)
+	{
+		if (temp1->next == NULL)
+			return (-1);
+	}
+/**
+*temp1 = temp1->next;
+*temp2 = temp1->next;
+*temp1->next = temp2->next;
+*temp2->next->prev = temp1;
+*free(temp2);
+*return (1);
+*/
+return (1);
 }
