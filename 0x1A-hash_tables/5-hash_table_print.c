a84583199c26c51a0eb13c863450645ddf33d35d
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-19 15:39:51 +0000

    print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,13 +3,13 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
-*hash_table_set- Function that adds an element to the hash table
+*hastable_set- Function that adds an element to the hash table
 *
 *
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	int len, i, hash;
+	int i;
 	hash_table_t *temp1;
 
 	temp1 = ht;
@@ -20,21 +20,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (key == NULL)
                 return (0);
-	else
-	{
 
-        	hash = hash_djb2(key);
-		index = hash %  temp1;
-	}
 	for (i = 0; i < temp1; i++)
 	{
-		if (i == index)
-		{
-			/*Index tiene algo agregar nuevo nodo*/
-			ht[i].value = strdup(value);
-			return (1);
-		}
-		else
-			return (0);
+		printf("/{%s : %s/}", ht[i].value, ht[i].key);
 	}
+	return (0);
 }
