1f918531e0e4765cc058c70719d529827758f3b9
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-20 01:12:52 +0000

    delete hash

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+/**
+*hash_table_delete- Function that deletes a hash table
+*@ht: hash table
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+
+
+
+}
