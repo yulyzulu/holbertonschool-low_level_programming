7e3b3c49fc03734e376bc6479b5b4aff682e34ec
Yulieth Zuluaga <yulyzulu05@gmail.com>
2020-01-20 01:02:07 +0000

    get value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include "hash_tables.h"
 /**
-*
-*
-*
-*
+*hash_table_get- Function that retrieves a value associated with a key
+*@ht: hash table
+*@key: key of a hash table
+*Return: value of a hash table
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
@@ -14,13 +14,15 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int hash, index = 0;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
-                return (0);
-        if (key == NULL || strlen(key) == 0)
-                return (0);
-        else
-        {
-                hash = hash_djb2((const unsigned char *)key);
-                index = hash %  ht->size;
+		return (0);
+	if (key == NULL || strlen(key) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		hash = hash_djb2((const unsigned char *)key);
+		index = hash %  ht->size;
 	}
 
 	temp1 = ht->array[index];
@@ -28,7 +30,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(temp1->key, key) == 0)
 		{
-			return temp1->value;
+			return (temp1->value);
 		}
 	temp1 = temp1->next;
 	}
