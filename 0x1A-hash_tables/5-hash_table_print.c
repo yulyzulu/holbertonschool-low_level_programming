1a18ea4431274f995ac7a740fb7be58e73d13787
yulyzulu <yulyzulu05@gmail.com>
2020-01-22 20:45:09 +0000

    print hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,13 +9,18 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned int i;
+	int a = 0;
 
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			printf("'%s' : '%s', ", ht->array[i]->key, ht->array[i]->value);
+			if (a == 1)
+				printf(", ");
+
+			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			a = 1;
 		}
 	}
 	printf("}\n");
